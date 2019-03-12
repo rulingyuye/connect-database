a64f1066076da6fe6499beff99c82ddbc32503c7
rulingyuye <rulingyuye@users.noreply.github.com>
2019-03-12 16:54:00 +0800

    连接MySQL并且不带ssh
    连接MySQL数据库，并且带ssh的，另外可以传入相应的参数

---
+++ connect_database_mysql_ssh.py
@@ -0,0 +1,25 @@
+#! -*- coding:utf-8 -*-
+
+
+def dbconn_database(sql_code,**kw):
+	'''
+	连接MySQL数据库，并且带ssh的，另外可以传入相应的参数
+	'''
+	try:
+		with SSHTunnelForwarder(
+				('ssh_host', 22312),  # B机器的配置
+				ssh_password='ssh_password',
+				ssh_username='ssh_username',
+				remote_bind_address=('remote_bind_address', 3306)) as server:  # A机器的配置
+			database = pymysql.connect(host='127.0.0.1',  # 此处必须是是127.0.0.1
+										 port=server.local_bind_port,
+										 user='user',
+										 passwd='passwd',
+										 db='db',
+										 charset='utf8')
+			cursor = database.cursor()
+			cursor.execute(sql_code.format(**kw))
+			data = cursor.fetchall()
+		return data
+	except Exception as e:
+		print(e)
