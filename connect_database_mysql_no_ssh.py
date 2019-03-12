db93568ed766687b94dbeb470b30c8f3e12d8200
rulingyuye <rulingyuye@users.noreply.github.com>
2019-03-12 16:46:36 +0800

    连接MySQL并且不带ssh
    连接MySQL并且不带ssh,并且进行参数传递的

---
+++ connect_database_mysql_no_ssh.py
@@ -0,0 +1,22 @@
+#! -*- coding:utf-8 -*-
+
+
+def dbconn_database(sql_code,**kw):
+	'''
+	连接MySQL数据库，并且不带ssh的，另外可以传入相应的参数
+	'''
+	try:
+		database = pymysql.connect(host='host',
+								 db='db',
+							   user='user',
+							 passwd='passwd',
+							   port=3306,
+							   charset='utf8')
+		cursor = database.cursor()
+		cursor.execute(sql_code.format(**kw))
+		data = cursor.fetchall()
+		database.close()
+		cursor.close()
+		return data
+	except Exception as e:
+		print(e)
